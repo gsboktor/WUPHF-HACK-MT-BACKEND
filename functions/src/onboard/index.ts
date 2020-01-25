a942158e6698c62b2da9452498689ff5c509398d
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2020-01-25 16:17:02 -0600

    finishes onboarding record creator

--- functions/src/onboard/index.ts
+++ functions/src/onboard/index.ts
@@ -2,15 +2,29 @@ import * as functions from "firebase-functions";
 const admin = require("firebase-admin");
 const db = admin.firestore();
 
+/*
+accepts in a UUID, username, email, carrier_string to create a record in the USERS collection
+*/
 export const handler = (
   req: functions.https.Request,
   res: functions.Response
-): void => {
-  let docRef = db.collection("USERS").doc("alovelace");
+) => {
+  const uuid = req.body.uuid;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const username = req.body.username;
+  const carrier = req.body.carrier;
+
+  let docRef = db.collection("USERS").doc(uuid);
 
   docRef.set({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
+    email: email,
+    phone: phone,
+    username: username,
+    carrier: carrier
+  });
+
+  return res.send({
+    status: "success"
   });
 };
