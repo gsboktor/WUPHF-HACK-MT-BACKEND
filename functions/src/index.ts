5bdb7384197083810167522659c269e5d5e55aa6
Elijah Dangerfield <elijahdangerfield111@gmail.com>
2020-01-25 14:15:02 -0600

    adds structure to calls

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,39 +1,6 @@
-import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import * as nodemailer from "nodemailer";
-const cors = require("cors")({ origin: true });
+import * as functions from "firebase-functions";
+import * as email from "./email";
 admin.initializeApp();
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "justwuphfit@gmail.com",
-    pass: "hackmt123"
-  }
-});
-exports.sendMail = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const dest = req.body.dest;
-    console.log(dest);
-    const message = req.body.message;
-    const from = req.body.from;
-
-    const mailOptions = {
-      from: "WUPHF <justwuphfit@gmail.com>",
-      to: dest,
-      subject: "WHUF from " + from,
-      text: message
-    };
-
-    return transporter.sendMail(mailOptions, (erro, _) => {
-      if (erro) {
-        return res.send(erro.toString());
-      }
-      return res.send({
-        status: "success"
-      });
-    });
-  });
-});
+exports.sendMail = functions.https.onRequest(email.handler);
